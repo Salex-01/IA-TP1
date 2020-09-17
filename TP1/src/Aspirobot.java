9605218edbf59291a3172873f5a38374fcdb269c
GUIVARCH <alan.guivarch.98860@gmail.com>
2020-09-17 16:59:46 +0200

    Aspirobot : run()

--- TP1/src/Aspirobot.java
+++ TP1/src/Aspirobot.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Aspirobot extends Thread {
     Sensors sensors;
     Effectors effectors;
@@ -16,6 +18,42 @@ public class Aspirobot extends Thread {
     @Override
     public void run() {
         while (!stopped) {
+            Random r;
+            r = new Random();
+            int rand = Math.abs(r.nextInt()) % 3;
+            if (rand == 0) {
+                int tmp = Math.abs(r.nextInt()) % 4;
+                if (tmp == 0) {
+                    if (this.posY > 0) {
+                        this.effectors.move('u');
+                    }
+                }
+                else if (tmp == 1) {
+                    if (this.posY < sensors.e.height - 1) {
+                        this.effectors.move('d');
+                    }
+                }
+                else if (tmp == 2) {
+                    if (this.posX > 0) {
+                        this.effectors.move('l');
+                    }
+                }
+                else {
+                    if (this.posX < sensors.e.width - 1) {
+                        this.effectors.move('r');
+                    }
+                }
+                System.out.println("move : " + this.effectors.e.score + "\n");
+                System.out.println("posX : " + this.posX + " posY : " + this.posY + "\n");
+            }
+            else if (rand == 1) {
+                System.out.println("pick : " + this.effectors.e.score + "\n");
+                this.effectors.pick();
+            }
+            else {
+                System.out.println("suck : " + this.effectors.e.score + "\n");
+                this.effectors.suck();
+            }
 
         }
     }
