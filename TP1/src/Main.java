f303c0ff587cc1e4ca7e53196f9804ae7933c461
Salex <salex@orange.fr>
2020-09-18 15:18:21 +0200

    Imports 2

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,8 +1,4 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.IOException;
 
 public class Main {
--- TP1/src/Sensors.java
+++ TP1/src/Sensors.java
@@ -1,4 +1,3 @@
-
 public class Sensors {
     Aspirobot bot;
     Environment e;
